e0edf7f06f753988b8ab886e2c4015b993e1c88c
Keegan Witt <keeganwitt@gmail.com>
2016-12-11 18:39:57 -0500

    tag with major Groovy version

--- buildImages.sh
+++ buildImages.sh
@@ -1,19 +1,20 @@
-groovyVersion=2.4.7
+groovyMajorVersion=2.4
+groovyVersion=${groovyMajorVersion}.7
 
 cd jre7
-docker build -t groovy:jre7-${groovyVersion} -t groovy:jre7-latest .
+docker build -t groovy:jre7-${groovyMajorVersion} groovy:jre7-${groovyVersion} -t groovy:jre7-latest .
 cd jdk7
-docker build -t groovy:jdk7-${groovyVersion} -t groovy:jdk7-latest .
+docker build -t groovy:jdk7-${groovyMajorVersion} groovy:jdk7-${groovyVersion} -t groovy:jdk7-latest .
 cd jre7-alpine
-docker build -t groovy:jre7-${groovyVersion}-alpine -t groovy:jre7-latest-alpine .
+docker build -t groovy:jre7-${groovyMajorVersion}-alpine groovy:jre7-${groovyVersion}-alpine -t groovy:jre7-latest-alpine .
 cd jdk7-alpine
-docker build -t groovy:jdk7-${groovyVersion}-alpine -t groovy:jdk7-latest-alpine .
+docker build -t groovy:jdk7-${groovyMajorVersion}-alpine groovy:jdk7-${groovyVersion}-alpine -t groovy:jdk7-latest-alpine .
 
 cd jre8
-docker build -t groovy:jre8-${groovyVersion} -t groovy:jre8-latest .
+docker build -t groovy:jre8-${groovyMajorVersion} groovy:jre8-${groovyVersion} -t groovy:jre8-latest .
 cd jdk8
-docker build -t groovy:jdk8-${groovyVersion} -t groovy:jdk8-latest .
+docker build -t groovy:jdk8-${groovyMajorVersion} groovy:jdk8-${groovyVersion} -t groovy:jdk8-latest .
 cd jre8-alpine
-docker build -t groovy:jre8-${groovyVersion}-alpine -t groovy:jre8-latest-alpine .
+docker build -t groovy:jre8-${groovyMajorVersion}-alpine groovy:jre8-${groovyVersion}-alpine -t groovy:jre8-latest-alpine .
 cd jdk8-alpine
-docker build -t groovy:jdk8-${groovyVersion}-alpine -t groovy:jdk8-latest-alpine .
+docker build -t groovy:jdk8-${groovyMajorVersion}-alpine groovy:jdk8-${groovyVersion}-alpine -t groovy:jdk8-latest-alpine .
