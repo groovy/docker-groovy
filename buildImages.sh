0c819d914bc1d4ebecbaaf6db6051109513fe890
Keegan Witt <keeganwitt@gmail.com>
2016-12-11 02:09:16 -0500

    GROOVY-7906 workaround

--- buildImages.sh
+++ buildImages.sh
@@ -4,18 +4,16 @@ cd jre7
 docker build -t groovy:jre7-${groovyVersion} -t groovy:jre7-latest .
 cd jdk7
 docker build -t groovy:jdk7-${groovyVersion} -t groovy:jdk7-latest .
-# commented out until https://issues.apache.org/jira/browse/GROOVY-7906 is resolved
-#cd jre7-alpine
-#docker build -t groovy:jre7-${groovyVersion}-alpine -t groovy:jre7-latest-alpine .
-#cd jdk7-alpine
-#docker build -t groovy:jdk7-${groovyVersion}-alpine -t groovy:jdk7-latest-alpine .
+cd jre7-alpine
+docker build -t groovy:jre7-${groovyVersion}-alpine -t groovy:jre7-latest-alpine .
+cd jdk7-alpine
+docker build -t groovy:jdk7-${groovyVersion}-alpine -t groovy:jdk7-latest-alpine .
 
 cd jre8
 docker build -t groovy:jre8-${groovyVersion} -t groovy:jre8-latest .
 cd jdk8
 docker build -t groovy:jdk8-${groovyVersion} -t groovy:jdk8-latest .
-# commented out until https://issues.apache.org/jira/browse/GROOVY-7906 is resolved
-#cd jre8-alpine
-#docker build -t groovy:jre8-${groovyVersion}-alpine -t groovy:jre8-latest-alpine .
-#cd jdk8-alpine
-#docker build -t groovy:jdk8-${groovyVersion}-alpine -t groovy:jdk8-latest-alpine .
+cd jre8-alpine
+docker build -t groovy:jre8-${groovyVersion}-alpine -t groovy:jre8-latest-alpine .
+cd jdk8-alpine
+docker build -t groovy:jdk8-${groovyVersion}-alpine -t groovy:jdk8-latest-alpine .
