afff987c0566348282147952fc9fe42ddea7fa6d
Keegan Witt <keeganwitt@gmail.com>
2017-01-16 01:02:23 -0500

    added some additional tags

--- buildImages.sh
+++ buildImages.sh
@@ -4,22 +4,78 @@ set -e
 groovyMajorVersion=2.4
 groovyVersion=${groovyMajorVersion}.8
 
-cd jre7
-docker build -t apache/groovy:jre7-${groovyMajorVersion} -t apache/groovy:jre7-${groovyVersion} -t apache/groovy:jre7-latest .
 cd ../jdk7
-docker build -t apache/groovy:jdk7-${groovyMajorVersion} -t apache/groovy:jdk7-${groovyVersion} -t apache/groovy:jdk7-latest .
-cd ../jre7-alpine
-docker build -t apache/groovy:jre7-${groovyMajorVersion}-alpine -t apache/groovy:jre7-${groovyVersion}-alpine -t apache/groovy:jre7-latest-alpine .
+docker build \
+     -t apache/groovy:jdk7-${groovyMajorVersion} \
+     -t apache/groovy:jdk7-${groovyVersion} \
+     -t apache/groovy:jdk7-latest \
+     .
+
+cd jre7
+docker build \
+     -t apache/groovy:jre7-${groovyMajorVersion} \
+     -t apache/groovy:jre7-${groovyVersion} \
+     -t apache/groovy:jre7-latest \
+     .
+
 cd ../jdk7-alpine
-docker build -t apache/groovy:jdk7-${groovyMajorVersion}-alpine -t apache/groovy:jdk7-${groovyVersion}-alpine -t apache/groovy:jdk7-latest-alpine .
+docker build \
+     -t apache/groovy:jdk7-${groovyMajorVersion}-alpine \
+     -t apache/groovy:jdk7-${groovyVersion}-alpine \
+     -t apache/groovy:jdk7-alpine \
+     .
+
+cd ../jre7-alpine
+docker build \
+     -t apache/groovy:jre7-${groovyMajorVersion}-alpine \
+     -t apache/groovy:jre7-${groovyVersion}-alpine \
+     -t apache/groovy:jre7-alpine \
+     .
+
+cd ../jdk8
+docker build \
+     -t apache/groovy:jdk8-${groovyMajorVersion} \
+     -t apache/groovy:jdk8-${groovyVersion} \
+     -t apache/groovy:jdk8-latest \
+     -t apache/groovy:jdk-${groovyMajorVersion} \
+     -t apache/groovy:jdk-${groovyVersion} \
+     -t apache/groovy:jdk-latest \
+     .
 
 cd ../jre8
-docker build -t apache/groovy:jre8-${groovyMajorVersion} -t apache/groovy:jre8-${groovyVersion} -t apache/groovy:jre8-latest .
-cd ../jdk8
-docker build -t apache/groovy:jdk8-${groovyMajorVersion} -t apache/groovy:jdk8-${groovyVersion} -t apache/groovy:jdk8-latest .
-cd ../jre8-alpine
-docker build -t apache/groovy:jre8-${groovyMajorVersion}-alpine -t apache/groovy:jre8-${groovyVersion}-alpine -t apache/groovy:jre8-latest-alpine .
+docker build \
+     -t apache/groovy:jre8-${groovyMajorVersion} \
+     -t apache/groovy:jre8-${groovyVersion} \
+     -t apache/groovy:jre8-latest \
+     -t apache/groovy:jre-${groovyMajorVersion} \
+     -t apache/groovy:jre-${groovyVersion} \
+     -t apache/groovy:jre-latest \
+     -t apache/groovy:${groovyMajorVersion} \
+     -t apache/groovy:${groovyVersion} \
+     -t apache/groovy:latest \
+     .
+
 cd ../jdk8-alpine
-docker build -t apache/groovy:jdk8-${groovyMajorVersion}-alpine -t apache/groovy:jdk8-${groovyVersion}-alpine -t apache/groovy:jdk8-latest-alpine .
+docker build \
+     -t apache/groovy:jdk8-${groovyMajorVersion}-alpine \
+     -t apache/groovy:jdk8-${groovyVersion}-alpine \
+     -t apache/groovy:jdk8-alpine \
+     -t apache/groovy:jdk-${groovyMajorVersion}-alpine \
+     -t apache/groovy:jdk-${groovyVersion}-alpine \
+     -t apache/groovy:jdk-alpine \
+     .
+
+cd ../jre8-alpine
+docker build \
+     -t apache/groovy:jre8-${groovyMajorVersion}-alpine \
+     -t apache/groovy:jre8-${groovyVersion}-alpine \
+     -t apache/groovy:jre8-alpine \
+     -t apache/groovy:jre-${groovyMajorVersion}-alpine \
+     -t apache/groovy:jre-${groovyVersion}-alpine \
+     -t apache/groovy:jre-alpine \
+     -t apache/groovy:${groovyMajorVersion}-alpine \
+     -t apache/groovy:${groovyVersion}-alpine \
+     -t apache/groovy:alpine \
+     .
 
 cd ..
