86719438a8e6fbde805f90c1b047bcc7234878f5
Keegan Witt <keeganwitt@gmail.com>
2017-03-03 00:30:19 -0500

    use long options

--- test/run.sh
+++ test/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-set -eu
+set -o errexit -o nounset
 
 image=$1
 expectedGroovyVersion=$2
 
-version=$(docker run --rm -v "${PWD}:/scripts" -w "/scripts" "${image}" groovy printVersion.groovy)
+version=$(docker run --rm --volume "${PWD}:/scripts" --workdir "/scripts" "${image}" groovy printVersion.groovy)
 if [ "${version}" != "${expectedGroovyVersion}" ]; then
     echo "version '${version}' does not match expected version '${expectedGroovyVersion}'"
     exit 1
