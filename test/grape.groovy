ab4e608802baae17b6910ad9d8d0fd094786fe81
Keegan Witt <keeganwitt@gmail.com>
2017-03-04 00:06:55 -0500

    added a test for Grape

---
+++ test/grape.groovy
@@ -0,0 +1,8 @@
+@Grab(group='org.apache.commons', module='commons-lang3', version='3.5')
+import org.apache.commons.lang3.StringUtils
+
+if (new File('/home/groovy/.groovy/grapes').listFiles()?.isEmpty()) {
+    System.exit 1
+} else {
+    System.exit 0
+}
