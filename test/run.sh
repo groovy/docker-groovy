bf426af3c93c9d254918311b32458fb57cefb7b8
Keegan Witt <keeganwitt@gmail.com>
2017-08-20 00:50:55 -0400

    use double bracket ifs

--- test/run.sh
+++ test/run.sh
@@ -5,13 +5,13 @@ image=$1
 expectedGroovyVersion=$2
 
 version=$(docker run --rm --volume "${PWD}:/scripts" --workdir "/scripts" "${image}" groovy printVersion.groovy)
-if [ "${version}" != "${expectedGroovyVersion}" ]; then
+if [[ "${version}" != "${expectedGroovyVersion}" ]]; then
     echo "version '${version}' does not match expected version '${expectedGroovyVersion}'"
     exit 1
 fi
 
 docker run --rm --volume "${PWD}:/scripts" --workdir "/scripts" "${image}" groovy grape.groovy
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
     echo "No Grape cached files found"
     exit 1
 fi
