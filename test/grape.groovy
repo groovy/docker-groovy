297ec8958c0070aaa8b048908de97818fe161071
Keegan Witt <keeganwitt@gmail.com>
2017-09-09 15:10:11 -0400

    update commons-lang3 to 3.6

--- test/grape.groovy
+++ test/grape.groovy
@@ -1,4 +1,4 @@
-@Grab(group='org.apache.commons', module='commons-lang3', version='3.5')
+@Grab(group='org.apache.commons', module='commons-lang3', version='3.6')
 import org.apache.commons.lang3.StringUtils
 
 if (new File(System.getProperty('user.home'), '.groovy/grapes').listFiles().size() == 0) {
