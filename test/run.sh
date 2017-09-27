93ff07afc65f34d612817f745aabad6bda93913a
Keegan Witt <keeganwitt@gmail.com>
2017-09-26 20:12:09 -0400

    symlink root .groovy to groovy .groovy for mounting volumes with uid != 1000

--- test/run.sh
+++ test/run.sh
@@ -4,13 +4,21 @@ set -o errexit -o nounset
 image=$1
 expectedGroovyVersion=$2
 
-version=$(docker run --rm --volume "${PWD}:/scripts" --workdir "/scripts" "${image}" groovy printVersion.groovy)
+if [[ $(id -u) -eq "1000" ]]; then
+    user="groovy"
+    home="/home/groovy"
+else
+    user="root"
+    home="/root"
+fi
+
+version=$(docker run --user "${user}" --rm --volume "${PWD}:${home}/scripts" --workdir "${home}/scripts" "${image}" groovy printVersion.groovy)
 if [[ "${version}" != "${expectedGroovyVersion}" ]]; then
     echo "version '${version}' does not match expected version '${expectedGroovyVersion}'"
     exit 1
 fi
 
-docker run --rm --volume "${PWD}:/scripts" --workdir "/scripts" "${image}" groovy grape.groovy
+docker run --user "${user}" --rm --volume "${PWD}:${home}/scripts" --workdir "${home}/scripts" "${image}" groovy grape.groovy
 if [[ $? -ne 0 ]]; then
     echo "No Grape cached files found"
     exit 1
