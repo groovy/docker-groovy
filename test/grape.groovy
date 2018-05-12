420f3af36206e1e6ddb1b715bfcc9f0591c8501f
Keegan Witt <keeganwitt@gmail.com>
2018-05-11 20:13:36 -0400

    update commons-lang in test to 3.7

--- test/grape.groovy
+++ test/grape.groovy
@@ -1,4 +1,4 @@
-@Grab(group='org.apache.commons', module='commons-lang3', version='3.5')
+@Grab(group='org.apache.commons', module='commons-lang3', version='3.7')
 import org.apache.commons.lang3.StringUtils
 
 if (new File('/home/groovy/.groovy/grapes').listFiles().size() == 0) {
