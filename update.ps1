3073c58eefb28e21ca166a5d2828840708380188
Keegan Witt <keeganwitt@gmail.com>
2022-06-05 19:21:13 -0400

    Fix copy/paste errors in update scripts

--- update.ps1
+++ update.ps1
@@ -3,4 +3,4 @@ $groovyVersion = $($args[0])
 dir -Recurse -Filter Dockerfile | ForEach-Object {
     (Get-Content -Path $_.FullName) -replace "ENV GROOVY_VERSION .+", "ENV GROOVY_VERSION ${groovyVersion}" | Set-Content $_.FullName
 }
-(Get-Content -Path .github/workflows/ci.yaml) -replace "expectedGradleVersion: .+", "expectedGradleVersion: ${gradleVersion}" | Set-Content .github/workflows/ci.yaml
+(Get-Content -Path .github/workflows/ci.yaml) -replace "expectedGroovyVersion: .+", "expectedGroovyVersion: ${groovyVersion}" | Set-Content .github/workflows/ci.yaml
--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ set -o errexit -o nounset -o pipefail
 groovyVersion=${1}
 
 sed --regexp-extended --in-place "s/ENV GROOVY_VERSION .+/ENV GROOVY_VERSION ${groovyVersion}/" ./*/Dockerfile
-sed --regexp-extended --in-place "s/expectedGradleVersion: .+$/expectedGradleVersion: ${gradleVersion}/" .github/workflows/ci.yaml
+sed --regexp-extended --in-place "s/expectedGroovyVersion: .+$/expectedGroovyVersion: ${groovyVersion}/" .github/workflows/ci.yaml
