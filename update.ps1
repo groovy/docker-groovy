e8413f81831a2aa484ef58a92a4e7dde876bf566
Keegan Witt <keeganwitt@gmail.com>
2024-09-27 18:10:06 -0400

    Replace legacy style env vars

--- update.ps1
+++ update.ps1
@@ -3,6 +3,6 @@ $groovyVersion = $(((Invoke-WebRequest "https://api.github.com/repos/apache/groo
 Write-Host "Updating to Groovy $groovyVersion"
 
 dir -Recurse -Filter Dockerfile | ForEach-Object {
-    (Get-Content -Path $_.FullName) -replace "ENV GROOVY_VERSION .+", "ENV GROOVY_VERSION ${groovyVersion}" | Set-Content $_.FullName
+    (Get-Content -Path $_.FullName) -replace "ENV GROOVY_VERSION=.+", "ENV GROOVY_VERSION=${groovyVersion}" | Set-Content $_.FullName
 }
 (Get-Content -Path .github/workflows/ci.yaml) -replace "expectedGroovyVersion: .+", "expectedGroovyVersion: ${groovyVersion}" | Set-Content .github/workflows/ci.yaml
--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ set -o errexit -o nounset -o pipefail
 groovyVersion=$(curl -s 'https://api.github.com/repos/apache/groovy/tags' | grep -Eo 'GROOVY_4.[0-9]{1,2}.[0-9]{1,2}' | head -n 1 | sed -e 's/GROOVY_//' -e 's/_/./g')
 echo "Updating to Groovy $groovyVersion"
 
-sed --regexp-extended --in-place "s/ENV GROOVY_VERSION .+/ENV GROOVY_VERSION ${groovyVersion}/" ./*/Dockerfile
+sed --regexp-extended --in-place "s/ENV GROOVY_VERSION=.+/ENV GROOVY_VERSION=${groovyVersion}/" ./*/Dockerfile
 sed --regexp-extended --in-place "s/expectedGroovyVersion: .+$/expectedGroovyVersion: ${groovyVersion}/" .github/workflows/ci.yaml
