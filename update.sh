d03577922e75c136e5fbdc85e7caf6ba147d4676
Keegan Witt <keeganwitt@gmail.com>
2024-11-12 19:17:51 -0500

    Compatibility with BSD sed

--- update.sh
+++ update.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
+_sed() {
+  if sed --version; then
+    # GNU sed
+    sed --regexp-extended --in-place "$@"
+  else
+    # BSD sed
+    sed -Ei '' "$@"
+  fi
+}
+
 groovyVersion=$(curl -s 'https://api.github.com/repos/apache/groovy/tags' | grep -Eo 'GROOVY_4.[0-9]{1,2}.[0-9]{1,2}' | head -n 1 | sed -e 's/GROOVY_//' -e 's/_/./g')
 echo "Updating to Groovy $groovyVersion"
 
-sed --regexp-extended --in-place "s/ENV GROOVY_VERSION=.+/ENV GROOVY_VERSION=${groovyVersion}/" ./*/Dockerfile
-sed --regexp-extended --in-place "s/expectedGroovyVersion: .+$/expectedGroovyVersion: ${groovyVersion}/" .github/workflows/ci.yaml
+_sed "s/ENV GROOVY_VERSION=.+/ENV GROOVY_VERSION=${groovyVersion}/" ./*/Dockerfile
+_sed "s/expectedGroovyVersion: .+$/expectedGroovyVersion: ${groovyVersion}/" .github/workflows/ci.yaml
