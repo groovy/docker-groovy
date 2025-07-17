2bea8f73261d6e0d7d77635c18757ddd902baf76
Keegan Witt <keeganwitt@gmail.com>
2025-07-16 22:26:47 -0400

    Set strict mode and add shebang

--- update.ps1
+++ update.ps1
@@ -1,3 +1,7 @@
+#!/usr/bin/env pwsh
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
 $groovyVersion = $(((Invoke-WebRequest "https://api.github.com/repos/apache/groovy/tags" | ConvertFrom-Json).name | Select-String -Pattern "GROOVY_4" | Select-Object -First 1).ToString().replace("GROOVY_", "").replace("_", "."))
 
 Write-Host "Updating to Groovy $groovyVersion"
